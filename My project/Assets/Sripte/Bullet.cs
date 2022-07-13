363f4d9a5b51ffd70a356f7a0c462f05e848ad44
kimss018 <kimss018@naver.com>
2022-07-13 16:42:11 +0900

    스크립트확인하기

---
+++ My project/Assets/Sripte/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    
+    private Vector3 direction;
+    
+    public void Shoot(Vector3 direction)
+    {
+        this.direction = direction;
+        // Destroy(gameObject, 5.0f);
+
+        Invoke("ReturnBulletToPooling", 5.0f);
+
+    }
+
+    public void ReturnBulletToPooling()
+    {
+        ObjectPool.ReturnObject(this);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction);
+    }
+}
