8c796aae2fc51489a9094d950e0c2424a007a870
kimss018 <106726051+kimss018@users.noreply.github.com>
2022-08-05 18:07:18 +0900

    좀비

--- Zombie_FPS/Assets/Scriptes/PlayerController.cs
+++ Zombie_FPS/Assets/Scriptes/PlayerController.cs
@@ -10,11 +10,30 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField]
     private float runSpeed;
+
+    [SerializeField]
+    private float crouchSpeed;
+
     private float applySpeed;
+
+    [SerializeField]
+    private float jumpForce;
+
 
 
     // 상태 변수
     private bool isRun = false;
+    private bool isCrouch = false;
+    private bool isGround = true;
+
+    // 앙ㅈ았을때 얼마나 앉을지 결정하는 변수
+    [SerializeField]
+    private float crouchPosY;
+    private float originPosY;
+    private float applyCrouchPosY;
+
+    // 땅 착지 여부
+    private CapsuleCollider capsuleCollider;
 
 
     // 카메라 민감도
@@ -37,19 +56,111 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        capsuleCollider = GetComponent<CapsuleCollider>();
         myRigid = GetComponent<Rigidbody>();
         applySpeed = walkSpeed;
+        originPosY = theCamera.transform.localPosition.y;
+        applyCrouchPosY = originPosY;
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        IsGround();
+        TryJump();
         TryRun();
+        TryCrouch();
         Move();
         CameraRotation();
         CharacterRotation();
     }
 
+    // 앉기 시도
+    private void TryCrouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Crouch();
+        }
+    }
+
+    // 앉기 동작
+    private void Crouch()
+    {
+        isCrouch = !isCrouch;
+
+          /*  if (isCrouch)
+                isCrouch = false;
+            else
+                isCrouch = true; */
+
+        if(isCrouch)
+        {
+            applySpeed = crouchSpeed;
+            applyCrouchPosY = crouchPosY;
+        }
+        else
+        {
+            applySpeed = walkSpeed;
+            applyCrouchPosY = originPosY;
+        }
+
+
+        // theCamera.transform.localPosition = new Vector3(theCamera.transform.localPosition.x, applyCrouchPosY, theCamera.transform.localPosition.z);
+        StartCoroutine(CrouchCoroution());    
+    }
+
+    // 부드러운 앉기 동작
+    IEnumerator CrouchCoroution()
+    {
+
+        float _posY = theCamera.transform.localPosition.y;
+        int count = 0;
+
+
+        while(_posY != applyCrouchPosY)
+        {
+            count++;
+
+            _posY = Mathf.Lerp(_posY, applyCrouchPosY, 0.3f);
+            theCamera.transform.localPosition = new Vector3(0, _posY, 0);
+            if (count > 15)
+                break;
+            
+            yield return null;  
+        }
+        theCamera.transform.localPosition = new Vector3(0, applyCrouchPosY, 0);
+
+        yield return new WaitForSeconds(1f);
+    }
+
+    // 지면 파악
+    private void IsGround()
+    {
+        isGround = Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y + 0.1f);
+    }
+
+    // 점프 실행
+    private void TryJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
+        {
+            Jump();
+        }
+    }
+
+    // 점프 실행
+    private void Jump()
+    {
+        // 앉은 상태에서 점프시 앉기 해제
+        if (isCrouch)
+            Crouch();
+
+        myRigid.velocity = transform.up * jumpForce;
+        
+    }
+    // 달리기 시도 
     private void TryRun()
     {
         if(Input.GetKey(KeyCode.LeftShift))
@@ -61,20 +172,24 @@ public class PlayerController : MonoBehaviour
             RunningCancel();
         }
     }
-
+    // 달리기 실행
     private void Running()
     {
+        // 앉은 상태에서 달리기 시 앉기 해제
+        if (isCrouch)
+            Crouch();
+
         isRun = true;
         applySpeed = runSpeed;
     }
-
+    // 달리기 취소
     private void RunningCancel()
     {
         isRun = false;
         applySpeed = walkSpeed;
     }
 
-
+    
     private void Move()
     {
         // 캐릭터 움직임
@@ -86,7 +201,7 @@ public class PlayerController : MonoBehaviour
 
         Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * applySpeed;
 
-        myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
+        myRigid.MovePosition(transform.position + _velocity * Time.fixedDeltaTime);
     }
 
     private void CharacterRotation()
